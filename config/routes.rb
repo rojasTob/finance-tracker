8698062f1bb63bcfb9c912f1d9282a8ef857765d
Angie Rojas <nataliart4@gmail.com>
2023-12-29 17:05:38 -0500

    Add users and stocks controller and add a search function

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "welcome#index"
+  get "my_portafolio", to: "users#my_portafolio"
+  get "search_stock", to: "stocks#search"
 end
