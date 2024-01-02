cc2261daf2c33f6e64fae547c0d4ca531879f14b
Angie Rojas <nataliart4@gmail.com>
2024-01-02 17:31:36 -0500

    Add first_name and last_name in User table, modify model and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configured_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configured_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
