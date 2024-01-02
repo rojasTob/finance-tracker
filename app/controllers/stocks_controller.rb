cb5a2a52f2bc44b94012ac4c87fe466b07bc2576
Angie Rojas <nataliart4@gmail.com>
2024-01-02 13:48:32 -0500

    Add partial for results, and try to render a div tag by ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,6 +5,10 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
 
       if @stock
+        # Try again to render only the part that needs to be updated with ajax: create a js template!!
+        # respond_to do | format |
+        #   format.js { render partial: 'users/result_js' }
+        # end
         render 'users/my_portafolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
