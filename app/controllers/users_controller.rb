5bfa5f51f62938d0006c7d27d04e21918ae15748
Angie Rojas <nataliart4@gmail.com>
2024-01-03 10:23:22 -0500

    Add friendship model, modify user model and controller, create view to display friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+
   def my_portafolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def my_friends
+    @friends = current_user.friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "my_portafolio", to: "users#my_portafolio"
   get "search_stock", to: "stocks#search"
+  get "my_friends", to: "users#my_friends"
 end
