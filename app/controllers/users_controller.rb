258646be42dba6c084a83bd77610f300d807f21f
Angie Rojas <nataliart4@gmail.com>
2024-01-03 11:38:15 -0500

    Implement search function and display results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    # Do this with ajax call to render just the partial js
+    @friends = current_user.friends
+    if params[:friend].present?
+      @friends_search = User.search(params[:friend])
+      @friends_search = current_user.except_current_user(@friends_search)
+      if @friends_search
+        render 'users/my_friends'
+      else
+        flash[:alert] = "Could not find friend"
+        redirect_to my_friends_path
+      end
+    else
+      flash[:alert] = "Please enter a friend name or email to search"
+      redirect_to my_friends_path
+    end
+  end
+
 end
